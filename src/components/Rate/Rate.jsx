856da00685f849460fd2d5f9ab6bcb8f11826a1a
Mariam <mariam.mantidze1@gmail.com>
2024-03-07 14:42:29 +0400

    wip: main card

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -1,10 +1,27 @@
 import Star from "/images/icon-star.svg";
+import "./rate.css";
 
 export default function Rate() {
+  let buttons = [1, 2, 3, 4, 5];
   return (
     <>
       <div className="rate-container">
-        <img src={Star} alt="icon of star" />
+        <div className="img-container">
+          <img src={Star} alt="icon of star" />
+        </div>
+
+        <h1>How did we do?</h1>
+        <p className="rate-text">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <div id="btns-container">
+          {buttons.map((button) => (
+            <button className="rate">{button}</button>
+          ))}
+        </div>
+
+        <button id="submit">Submit</button>
       </div>
     </>
   );
